6f014da94789f0ec776cba0a0eb703c9ad90232c
Bulat Aykaev <mefcorvi@gmail.com>
2021-11-15 15:24:17 +0300

    fix: Better typescript declaration

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,22 +1,53 @@
-enum APIVersion {
-  V1 = "V1",
-}
-
-interface GreenSMSRequestConfig {
-  username?: string;
-  password?: string;
+export interface GreenSMSOptions {
+  user?: string;
+  pass?: string;
   token?: string;
   useTokenForRequests?: boolean;
-  version?: APIVersion;
+  version?: "V1";
+  camelCaseResponse?: boolean;
 }
 
-declare class GreenSMS {
+export interface GreenSMSGet {
+  <T>(params?: any): Promise<T>;
+}
+
+export interface GreenSMSPost {
+  <T>(params: any): Promise<T>;
+}
+
+export type GreenSMSModule<TGet extends keyof any, TPost extends keyof any> = {
+  [P in TGet]: GreenSMSGet;
+} & {
+  [P2 in TPost]: GreenSMSPost;
+};
+
+export declare class GreenSMS {
   /**
    * Creates a GreenSMS Client Object
    * @constructor
    * @param GreenSMSOptions
    */
-  constructor(GreenSMSOptions);
+  constructor(opts: GreenSMSOptions);
+
+  account: GreenSMSModule<"balance" | "tarif", "token">;
+
+  call: GreenSMSModule<"status", "send">;
+
+  hlr: GreenSMSModule<"status", "send">;
+
+  pay: GreenSMSModule<"status", "send">;
+
+  sms: GreenSMSModule<"status", "send">;
+
+  viber: GreenSMSModule<"status", "send">;
+
+  voice: GreenSMSModule<"status", "send">;
+
+  social: GreenSMSModule<"status", "send">;
+
+  general: GreenSMSModule<"status", never>;
+
+  whois: GreenSMSModule<"lookup", never>;
 }
 
-export { GreenSMS, GreenSMSRequestConfig };
+export default GreenSMS;
